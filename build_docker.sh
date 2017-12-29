275330c199c07b1cfaa16dd5ef3523e7c206faeb
Arnav Gupta <arnav@codingblocks.com>
2017-12-29 20:37:48 +0530

    make tagged builds
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- build_docker.sh
+++ build_docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+VERSION=$(node -p -e "require('./package.json').version")
+
 npm install -D
 npm run build
 
-docker build -t codingblocks/judge-api .
+docker build -t codingblocks/judge-api:$VERSION .
 docker system prune -f
 
 read -p "Push to Docker? [Press Enter to continue]"
-docker push codingblocks/judge-api
+docker push codingblocks/judge-api:$VERSION
