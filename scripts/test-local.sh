ee381c677b4f2730593e09e53f175108a9053254
Arnav Gupta <arnav@codingblocks.com>
2017-12-30 04:07:18 +0530

    wait longer
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- scripts/test-local.sh
+++ scripts/test-local.sh
@@ -6,7 +6,7 @@ curl -L https://raw.githubusercontent.com/coding-blocks/judge-compose/master/jud
 docker build -t codingblocks/judge-api .
 docker-compose -p judgecompose up -d
 
-docker exec judgecompose_api_1 scripts/wait-for-it.sh api:3737 -- npm run seedlangs
+docker exec judgecompose_api_1 scripts/wait-for-it.sh -t 180 api:3737 -- npm run seedlangs
 docker exec -t --env "JUDGEAPI_HOST=api JUDGEAPI_PORT=2222" judgecompose_api_1  pkill node && DEBUG=test:* npm run test
 
 docker-compose kill
