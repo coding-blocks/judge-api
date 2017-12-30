5514befbb091f2de178fc090b048287e820f9323
Arnav Gupta <arnav@codingblocks.com>
2017-12-30 12:51:50 +0530

    test only this, without other judge-* deps
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- scripts/test-local.sh
+++ scripts/test-local.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-curl -L https://raw.githubusercontent.com/coding-blocks/judge-compose/master/docker-compose-withdb.yml > docker-compose.yml
-curl -L https://raw.githubusercontent.com/coding-blocks/judge-compose/master/judgeapi-example.env > .env
-
-docker build -t codingblocks/judge-api .
+docker-compose build
 docker-compose -p judgecompose up -d
 
 docker exec judgecompose_api_1 scripts/wait-for-it.sh -t 180 api:3737 -- npm run seedlangs
 docker kill judgecompose_api_1
-docker run -t --env-file .env \
+docker run -t --env-file test.env \
         --network judgecompose_default \
         --env "DEBUG=test:* JUDGEAPI_HOST=api JUDGEAPI_PORT=2222" codingblocks/judge-api \
         npm run cover
 
 docker-compose kill
 docker-compose down
-
-rm docker-compose.yml
-rm .env
 
 docker system prune -f
 docker volume prune -f
---
+++ test/utils/setup-mock-queue.ts
@@ -0,0 +1,31 @@
+import {RunResponse} from '../../src/routes/api/run'
+import * as amqp from 'amqplib/callback_api'
+import {Channel, Connection} from 'amqplib/callback_api'
+import app, {config} from '../../src/server'
+
+const jobQ = 'job_queue'
+const successQ = 'success_queue'
+
+before((done) => {
+  amqp.connect(`amqp://${config.AMQP.USER}:${config.AMQP.PASS}@${config.AMQP.HOST}:${config.AMQP.PORT}`,
+    (err, connection) => {
+      if (err) throw err
+
+      connection.createChannel((err2, channel) => {
+
+        channel.assertQueue(successQ)
+        channel.assertQueue(jobQ)
+        channel.consume(jobQ, (msg) => {
+          let job = JSON.parse(msg.content.toString())
+          channel.sendToQueue(successQ, (new Buffer(JSON.stringify(<RunResponse>{
+            id: job.id,
+            stderr: 'stderr',
+            stdout: 'stdout'
+          }))))
+          channel.ack(msg)
+        })
+
+
+      })
+    })
+})
