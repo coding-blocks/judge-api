7cb4a781e6f8a2451639591ec9b18060803526a9
Arnav Gupta <dev@championswimmer.in>
2018-05-24 19:37:28 +0530

    verbose-fy seedlands

--- src/db/scripts/seed-defaultlangs.ts
+++ src/db/scripts/seed-defaultlangs.ts
@@ -2,6 +2,7 @@ import {ApiKeyAttrs, ApiKeys, db, Langs, LangsAttributes} from '../models'
 
 db.sync()
   .then(() => Langs.sync({force:true}))
+  .then(async () => {console.log('Languages Synced')})
   .then(() =>
     Langs.bulkCreate(<LangsAttributes[]>[
       { lang_slug: 'py2', lang_name: 'Python', lang_version: '2.7' },
@@ -10,11 +11,15 @@ db.sync()
       { lang_slug: 'cpp', lang_name: 'C++', lang_version: '11' },
       { lang_slug: 'c', lang_name: 'C', lang_version: '6' }
     ]))
+  .then(async () => {console.log('Languages Seeded')})
   .then(() => ApiKeys.sync({force: true}))
+  .then(async () => {console.log('API Keys Synced')})
   .then(() =>
     ApiKeys.bulkCreate(<ApiKeyAttrs[]>[
       {id: 1, key: '7718330d2794406c980bdbded6c9dc1d', whitelist_domains: ['*'], whitelist_ips: ['*']}
     ]))
+  .then(async () => {console.log('API Keys Seeded')})
+  .catch(console.error)
   .finally(() => {
     try {
       db.close()
