49c186e91222e16c11e6a45f7826c7f8b6006667
Muhammad Farhan <farhanshah.96930012@gmail.com>
2019-05-18 18:29:17 +0530

    raven support added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express = require('express')
 import dbg = require('debug')
 import path = require ('path')
+import Raven = require('raven')
 import apiRoute from './routes/api'
 
+const DSN = process.env.DSN
 const debug = dbg('server:main')
-
 const app = express()
+
+Raven.config(DSN).install()
+app.use(Raven.requestHandler())
+app.use(Raven.errorHandler())
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/docs', express.static(path.join(__dirname, '../docs')))
