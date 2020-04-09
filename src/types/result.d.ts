6b43987eba71c888bee365ab605516ecb123bbe7
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-04-09 18:40:09 +0530

    response types update
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/types/result.d.ts
+++ src/types/result.d.ts
@@ -1,6 +1,21 @@
-export interface Testcase { 
-  id: number, 
-  score: number, 
-  time: string, 
-  result: string 
+export interface TestcaseResult {
+  id: number,
+  score: number,
+  time: string,
+  result: string
+}
+
+export interface Result {
+  id: number,
+  stderr: string,
+}
+
+export interface RunResult extends Result {
+  stdout: string,
+  time: number,
+  code: number
+}
+
+export interface SubmissionResult extends Result {
+  testcases: Array<TestcaseResult>
 }
