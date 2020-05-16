3a8bae3001ad0763a6c6d9ea6d0a55d582e0bd44
PrabalSingh <praba170107045@iitg.ac.in>
2020-05-16 10:36:21 +0530

    feature(project): add ProjectValidator

---
+++ src/routes/api/project/validators.ts
@@ -0,0 +1,34 @@
+import { Handler } from 'express'
+import BaseValidator from 'validators/baseValidator'
+import * as Joi from '@hapi/joi'
+
+export default class ProjectValidator extends BaseValidator {
+    POST: Handler
+
+    constructor() {
+        super()
+        this.POST = this.requestValidator(this.POSTSchema)
+    }
+
+    POSTSchema = Joi.object({
+        problem: Joi
+            .string()
+            .uri()
+            .required(),
+        submission: Joi
+            .string()
+            .uri()
+            .required(),
+        submissionDirs: Joi
+            .array(),
+        mode: Joi
+            .string()
+            .valid('sync', 'callback', 'poll'),
+        callback: Joi
+            .string()
+            .uri()
+            .when('mode', { is: 'callback', then: Joi.required() }),
+        timelimit: Joi
+            .number(),
+    })
+}
