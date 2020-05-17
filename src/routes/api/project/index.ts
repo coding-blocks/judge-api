c5206f6d4ff7bf59d275f031154aba23ed6a7c24
PrabalSingh <praba170107045@iitg.ac.in>
2020-05-17 09:53:07 +0530

    chore(project): add index

---
+++ src/routes/api/project/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import Controller from './controller'
+import Validator from './validators'
+import { successListener } from 'rabbitmq/jobqueue'
+
+const router: Router = Router()
+const validator = new Validator()
+
+router.post('/', validator.POST, Controller.runPOST)
+successListener.on('project_result', Controller.onSuccess)
+
+export default router
