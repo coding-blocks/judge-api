eb1f41f34182d9fe73b2bd59d19ada2adc3a001d
PrabalSingh <praba170107045@iitg.ac.in>
2020-05-20 19:53:52 +0530

    fix minor bugs

--- src/routes/api/project/index.ts
+++ src/routes/api/project/index.ts
@@ -2,11 +2,12 @@ import { Router } from 'express'
 import Controller from './controller'
 import Validator from './validators'
 import { successListener } from 'rabbitmq/jobqueue'
+import langValidator from '../../../middlewares/langValidator';
 
 const router: Router = Router()
 const validator = new Validator()
 
-router.post('/', validator.POST, Controller.runPOST)
+router.post('/', validator.POST, langValidator(), Controller.runPOST)
 successListener.on('project_result', Controller.onSuccess)
 
 export default router
