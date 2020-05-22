3853582d8502d2a057b08b51c8314f7740ff6c24
PrabalSingh <praba170107045@iitg.ac.in>
2020-05-22 13:18:32 +0530

    feat(poll): add endpoint for polling

---
+++ src/routes/api/result/controller.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express";
+import DB from 'models'
+
+export default {
+    async sendResult(req: Request, res: Response) {
+        const submissionId = req.params.id ? parseInt(req.params.id) : null
+        if (!submissionId) {
+            res.status(400).json({err: 'SubmissionId not found'})
+        }
+        else {
+            DB.submissions.findByPk(submissionId)
+                .then((submission) => {
+                    res.status(200).json(submission.results)
+                }).catch((err) => {
+                    res.status(404).json({err: 'Submission not found'})
+                })
+        }
+    }
+}
