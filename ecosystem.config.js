addd4557f0a931fc069d3a5f7f7449390f3c6879
Jatin Katyal <jatin.katyal13@gmail.com>
2020-06-12 20:42:29 +0530

    fix: Node path issue with pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,11 @@ module.exports = {
     name: 'Judge2 API',
     script: 'dist/run.js',
     autorestart: true,
+    interpreter: '/home/codingblocks/.nvm/versions/node/v10.21.0/bin/node',
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development',
+      NODE_PATH: 'dist',
       ...require('./secrets.json')
     },
     env_production: {
